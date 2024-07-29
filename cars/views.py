4851f0bfed1bbb3b65e1f916c1066d0f0b7da102
Fernando Carvalho <84752008+Fernandoafsc@users.noreply.github.com>
2024-07-29 18:58:17 -0300

    Adicionando Search.

--- cars/views.py
+++ cars/views.py
@@ -3,7 +3,10 @@ from cars.models import Car
 
 
 def cars_view(request):
-    cars = Car.objects.filter(model__contains='Chevette') 
+    cars = Car.objects.all().order_by('model') 
+    search = request.GET.get('search')    
+    if search:
+        cars = cars.filter(model__icontains=search)
     
     return render(
         request,
