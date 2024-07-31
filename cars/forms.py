d0808c140f3bcc8fc2f1dca050e082e2e37e3d4d
Fernando Carvalho <84752008+Fernandoafsc@users.noreply.github.com>
2024-07-30 21:10:20 -0300

    Crianlação do Django Form e validação do Form.

--- cars/forms.py
+++ cars/forms.py
@@ -1,12 +1,21 @@
 from django import forms
-from cars.models import Brand
+from cars.models import Brand, Car
 
-class CarForm(forms.Form):
-    model = forms.CharField(max_length=200)
-    brand = forms.ModelChoiceField(Brand.objects.all())
-    factory_year = forms.IntegerField()
-    model_year = forms.IntegerField()
-    plate = forms.CharField(max_length=10)
-    value = forms.FloatField()
-    photo = forms.ImageField()
+ 
+class CarModelForm(forms.ModelForm):
+    class Meta:
+        model = Car
+        fields = '__all__'
     
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+        if value < 20000:
+            self.add_error('value', 'Valor mínimo do carro deve ser R$20.000')
+        return value
+    def clean_factory_year(self):
+        factory_year = self.cleaned_data.get('factory_year')
+        if factory_year < 1975:
+            self.add_error('factory_year', 'Não é possível cadastrar carros fabricados antes de 1975.')
+        return factory_year
+    
+            
